cb7610956c8d28dddd71276a8cbb7d085f9b898f
André Tapia <atapia@webnet.fr>
2018-03-28 10:27:00 +0200

    - Montée de version de jasig/phpcas pour corrigé les pb de sécurité - Désactivation des logs (trop verbeu pour la production)

--- Buzz/CasClient.php
+++ Buzz/CasClient.php
@@ -5,6 +5,7 @@
  * Date: 14/01/2016
  * Time: 17:17
  */
+
 namespace Webnet\SsoAuthBundle\Buzz;
 
 use Buzz\Client\ClientInterface;
@@ -12,16 +13,29 @@ use Buzz\Exception\ClientException;
 use Buzz\Message\MessageInterface;
 use Buzz\Message\RequestInterface;
 
+/**
+ * Class CasClient
+ *
+ * @package Webnet\SsoAuthBundle\Buzz
+ */
 class CasClient implements ClientInterface
 {
     private $client;
     private $options;
 
+    /**
+     * CasClient constructor.
+     *
+     * @param array $options
+     */
     public function __construct(array $options = array())
     {
         $this->options = $options;
         \phpCAS::getVersion();
-        \phpCAS::setDebug('/tmp/cas-log.log');
+
+        // Désactivé car trop verbeux pour la production...
+        // \phpCAS::setDebug('/tmp/cas-log.log');
+
         \phpCAS::setVerbose(true);
         $this->client = new \CAS_Client(
             SAML_VERSION_1_1,
@@ -34,6 +48,10 @@ class CasClient implements ClientInterface
         $this->client->handleLogoutRequests(false, false);
     }
 
+    /**
+     * @param RequestInterface $request
+     * @param MessageInterface $response
+     */
     public function send(RequestInterface $request, MessageInterface $response)
     {
         try {
@@ -43,5 +61,4 @@ class CasClient implements ClientInterface
 
         }
     }
-
 }
